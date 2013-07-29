61439263151f764243b51d0c4562871859953ba2
LittleDing <dingweijie3000@qq.com>
2013-07-29 23:09:09 +0800

    finish Path_Sum_II #1

---
+++ Path_Sum_II.cpp
@@ -0,0 +1,38 @@
+//Attemp #1
+//from 	2013-7-29 23:00
+//to	2013-7-29 23:06
+//failed before small AC: CE*1 + RTE*0 + WA*1
+//failed before large AC: None
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    void dfs(TreeNode *root, int sum,vector<int> &path,vector<vector<int> > &ans){
+        if(!root) return ;
+        path.push_back(root->val); 
+        sum -= root->val;
+        if(!root->left && !root->right){
+            if(sum==0) ans.push_back(path);
+        }else {
+            dfs(root->left,sum,path,ans);
+            dfs(root->right,sum,path,ans);
+        }
+        path.pop_back();
+    }
+public:
+    vector<vector<int> > pathSum(TreeNode *root, int sum) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        vector<vector<int> > ans;
+        vector<int> path;
+        dfs(root,sum,path,ans);
+        return ans;
+    }
+};
