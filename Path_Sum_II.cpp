c9758c2a11a492f89912d1f75be68b6ce09ead51
LittleDing <dingweijie3000@qq.com>
2013-08-07 00:46:34 +0800

    finished Path_Sum_II #1

--- Path_Sum_II.cpp
+++ Path_Sum_II.cpp
@@ -1,8 +1,9 @@
 //Attemp #1
-//from 	2013-7-29 23:00
-//to	2013-7-29 23:06
-//failed before small AC: CE*1 + RTE*0 + WA*1
+//from 	2013-8-7 0:39
+//to	2013-8-7 0:45
+//failed before small AC: CE*1 + RTE*0 + WA*0
 //failed before large AC: None
+
 
 /**
  * Definition for binary tree
@@ -14,15 +15,16 @@
  * };
  */
 class Solution {
-    void dfs(TreeNode *root, int sum,vector<int> &path,vector<vector<int> > &ans){
-        if(!root) return ;
-        path.push_back(root->val); 
+    void dfs(TreeNode *root, int sum, vector<int> &path,vector<vector<int>> &ans){
+        path.push_back(root->val);
         sum -= root->val;
         if(!root->left && !root->right){
-            if(sum==0) ans.push_back(path);
-        }else {
-            dfs(root->left,sum,path,ans);
-            dfs(root->right,sum,path,ans);
+            if(sum==0){
+                ans.push_back(path);
+            }
+        }else{
+            if(root->left) dfs(root->left,sum,path,ans);
+            if(root->right) dfs(root->right,sum,path,ans);
         }
         path.pop_back();
     }
@@ -30,9 +32,10 @@ public:
     vector<vector<int> > pathSum(TreeNode *root, int sum) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        vector<vector<int> > ans;
+        vector<vector<int>> ans;
         vector<int> path;
-        dfs(root,sum,path,ans);
+        if(root)
+            dfs(root,sum,path,ans);
         return ans;
     }
 };
